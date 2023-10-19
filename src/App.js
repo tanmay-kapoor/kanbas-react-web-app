edac3a6451fbaf19bf2a850e832c4394f9500fd1
tanmaykapoor <tanmay.skater@gmail.com>
2023-10-19 09:08:37 -0400

    might have fixed problem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import Kanbas from './Kanbas';
-import {HashRouter, BrowserRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
+import Kanbas from "./Kanbas";
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
 
+import Labs from "./Labs";
+import HelloWorld from "./Labs/a3/HelloWorld";
 
-import Labs from './labs';
-import HelloWorld from './labs/a3/HelloWorld';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    // <HashRouter>
-    <BrowserRouter>
-        <Routes>
-          <Route path="/"         element={<Navigate to="/labs"/>}/>
-          <Route path="/hello"    element={<HelloWorld/>}/>
-          <Route path="/labs/*"   element={<Labs/>}/>
-          <Route path="/kanbas/*" element={<Kanbas/>}/>
-        </Routes>
-    </BrowserRouter>
-  );
+    return (
+        // <HashRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/labs" />} />
+                <Route path="/hello" element={<HelloWorld />} />
+                <Route path="/labs/*" element={<Labs />} />
+                <Route path="/kanbas/*" element={<Kanbas />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
