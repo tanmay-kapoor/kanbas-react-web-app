067a42708cd96660a48018a5211d4c622d401129
tanmaykapoor <tanmay.skater@gmail.com>
2023-10-27 15:16:15 -0400

    refactor no assignments created

--- src/Kanbas/courses/assignments/index.js
+++ src/Kanbas/courses/assignments/index.js
@@ -25,14 +25,7 @@ function Assignments() {
         a.course !== courseId ? dispatch(deleteAssignment(a._id)) : a
     );
 
-    return assignments.length === 0 ? (
-        <div
-            className="col width-100 alert alert-danger height-100"
-            role="alert"
-        >
-            No assignments created
-        </div>
-    ) : (
+    return (
         <div class="col width-100">
             <div class="row">
                 <div class="col">
@@ -62,61 +55,77 @@ function Assignments() {
 
             <hr class="mt-3" />
 
-            <ul class="list-group assignments">
-                <li class="list-group-item list-group-item-secondary">
-                    Assignments
-                    <i
-                        class="fa fa-ellipsis-v float-end ms-4 color-gray"
-                        aria-hidden="true"
-                    ></i>
-                    <i class="fa fa-plus float-end ms-3" aria-hidden="true"></i>
-                    <label class="custom-label float-end">40% of Total</label>
-                </li>
+            {assignments.length === 0 ? (
+                <div
+                    className="center-align col width-100 alert alert-danger"
+                    role="alert"
+                >
+                    No assignments created
+                </div>
+            ) : (
+                <ul class="list-group assignments">
+                    <li class="list-group-item list-group-item-secondary">
+                        Assignments
+                        <i
+                            class="fa fa-ellipsis-v float-end ms-4 color-gray"
+                            aria-hidden="true"
+                        ></i>
+                        <i
+                            class="fa fa-plus float-end ms-3"
+                            aria-hidden="true"
+                        ></i>
+                        <label class="custom-label float-end">
+                            40% of Total
+                        </label>
+                    </li>
 
-                {assignments.map((assignment) => {
-                    return (
-                        <li class="list-group-item sub-heading">
-                            <div class="d-flex ass-box align-items-center">
-                                <i class="fa-solid fa-grip-vertical color-gray"></i>
-                                <i class="fa-regular fa-pen-to-square color-gray ms-3"></i>
-                                <div class="ass-description ms-3">
-                                    <Link
-                                        to={assignment._id}
-                                        element={<AssignmentEditor />}
-                                        onClick={() => {
-                                            dispatch(setAssignment(assignment));
+                    {assignments.map((assignment) => {
+                        return (
+                            <li class="list-group-item sub-heading">
+                                <div class="d-flex ass-box align-items-center">
+                                    <i class="fa-solid fa-grip-vertical color-gray"></i>
+                                    <i class="fa-regular fa-pen-to-square color-gray ms-3"></i>
+                                    <div class="ass-description ms-3">
+                                        <Link
+                                            to={assignment._id}
+                                            element={<AssignmentEditor />}
+                                            onClick={() => {
+                                                dispatch(
+                                                    setAssignment(assignment)
+                                                );
+                                            }}
+                                        >
+                                            {assignment.title}
+                                        </Link>
+                                        <p>
+                                            <strong>Due</strong> Sep 18 at
+                                            11:59pm | -/100 pts
+                                        </p>
+                                    </div>
+                                    <i
+                                        class="fa fa-check-circle ms-auto color-green"
+                                        aria-hidden="true"
+                                    ></i>
+                                    <i
+                                        class="fa fa-trash float-end ms-4  color-red"
+                                        aria-hidden="true"
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            dispatch(
+                                                deleteAssignment(assignment._id)
+                                            );
                                         }}
-                                    >
-                                        {assignment.title}
-                                    </Link>
-                                    <p>
-                                        <strong>Due</strong> Sep 18 at 11:59pm |
-                                        -/100 pts
-                                    </p>
+                                    ></i>
+                                    <i
+                                        class="fa fa-ellipsis-v float-end ms-4  color-gray"
+                                        aria-hidden="true"
+                                    ></i>
                                 </div>
-                                <i
-                                    class="fa fa-check-circle ms-auto color-green"
-                                    aria-hidden="true"
-                                ></i>
-                                <i
-                                    class="fa fa-trash float-end ms-4  color-red"
-                                    aria-hidden="true"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        dispatch(
-                                            deleteAssignment(assignment._id)
-                                        );
-                                    }}
-                                ></i>
-                                <i
-                                    class="fa fa-ellipsis-v float-end ms-4  color-gray"
-                                    aria-hidden="true"
-                                ></i>
-                            </div>
-                        </li>
-                    );
-                })}
-            </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
         </div>
     );
 }
